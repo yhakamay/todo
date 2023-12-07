37f011fbac1694cdd4739c7a5575d1b8cbd157fa
Yusuke <yhakamaya@adobe.com>
2023-12-07 18:11:15 +0900

    add new todo field and add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
       <h1>Todo</h1>
+      <div className="flex flex-row w-full max-w-lg gap-2">
+        <input
+          type="text"
+          placeholder="Type new todo"
+          className="input input-bordered w-full"
+        />
+        <button className="btn btn-primary">Add</button>
+      </div>
     </main>
   );
 }
