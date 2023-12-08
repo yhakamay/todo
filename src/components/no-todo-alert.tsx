2cc978afd3380c1ba4016c842854c849958ecf2a
Yusuke <yhakamaya@adobe.com>
2023-12-08 10:55:29 +0900

    update no-todo-alert.tsx
    use Alert component from daisyUI instead of Card

--- src/components/no-todo-alert.tsx
+++ src/components/no-todo-alert.tsx
@@ -6,10 +6,21 @@ export default function NoTodoAlert(props: noTodoAlertProps) {
   const { message } = props;
 
   return (
-    <div className="text-center border border-base-content card bg-base-100">
-      <div className="card-body">
-        <h2 className="text-xl">{message}</h2>
-      </div>
+    <div role="alert" className="alert">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="stroke-info shrink-0 w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+      <span>{message}</span>
     </div>
   );
 }
