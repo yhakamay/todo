9bf94a03a67585ad793cf316e0ba27bdf6d6c344
Yusuke <yhakamaya@adobe.com>
2023-12-08 14:35:51 +0900

    update imports
    import from geist/font/sans and /mono instead of geist/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { GeistMono, GeistSans } from "geist/font";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 
 export const metadata: Metadata = {
