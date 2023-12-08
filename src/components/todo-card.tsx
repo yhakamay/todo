05933e3ffa7b808413e14f6022672cd72d143bab
Yusuke <yhakamaya@adobe.com>
2023-12-08 11:16:47 +0900

    add complete button to TodoCard

--- src/components/todo-card.tsx
+++ src/components/todo-card.tsx
@@ -16,6 +16,23 @@ export default function TodoCard({ todo }: { todo: Todo }) {
           <div className="badge">once</div>
         )}
         <h2 className="card-title">{todo.title}</h2>
+        <div className="card-actions justify-end">
+          <button className="btn btn-square btn-outline" title="complete">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
