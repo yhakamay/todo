60e710cdba8de8924f43d365c2e771ade2caf028
Yusuke Hakamaya <yhakamaya1031@gmail.com>
2023-12-24 22:05:05 +0900

    add timeline

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -39,7 +39,54 @@ export default function TodoDetails(props: TodoDetailsProps) {
   return (
     <>
       <h1>{todo.title}</h1>
-      <p>{todo.description}</p>
+      {todo.description && todo.description.length > 0 && <p>Description</p>}
+      {todo.completedDates !== null && todo.completedDates.length > 0 && (
+        <ul className="timeline not-prose">
+          <li>
+            <div className="timeline-start">
+              {todo.createdAt.toLocaleDateString()}
+            </div>
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <hr />
+          </li>
+          {todo.completedDates.map((completedDate, index) => (
+            <li key={completedDate.toISOString()}>
+              <hr />
+              <div className="timeline-start">
+                {completedDate.toLocaleDateString()}
+              </div>
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              {index !== todo.completedDates!.length - 1 && <hr />}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
